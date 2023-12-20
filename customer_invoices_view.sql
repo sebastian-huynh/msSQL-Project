19807b7695cd5dd83b8a64131dcb81f3b0b5159f
Sebastian Huynh <147793356+sebastian-huynh@users.noreply.github.com>
2023-12-20 01:47:57 -0800

    Delete customer_invoices_view.sql

--- customer_invoices_view.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW customer_invoice
-AS SELECT customers.customer_id, customers.customer_first_name, customers.customer_last_name,
-customers.customer_address, orders.order_id, orders.order_date, orders.shipped_date,
-order_details.order_qty, items.item_id, items.title, items.unit_price
-FROM customers
-INNER JOIN orders ON customers.customer_id = orders.customer_id
-INNER JOIN order_details ON orders.order_id = order_details.order_id
-INNER JOIN items ON order_details.item_id = items.item_id;
